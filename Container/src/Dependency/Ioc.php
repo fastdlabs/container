f952b9546eede13f7691fc308984a84831bca931
janhuang <384099566@qq.com>
2015-01-26 21:16:05 +0800

    test

---
+++ Container/src/Dependency/Ioc.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: janhuang
+ * Date: 15/1/26
+ * Time: 下午1:05
+ * Github: https://www.github.com/janhuang 
+ * Coding: https://www.coding.net/janhuang
+ * sf: http://segmentfault.com/u/janhuang
+ * Blog: http://segmentfault.com/blog/janhuang
+ */
+
+namespace Container\Dependency;
+
+class Ioc 
+{
+    private $bundle;
+
+    private $reflection;
+
+    public function __construct($bundle)
+    {
+        $this->bundle = $bundle;
+
+        $this->reflection = new \ReflectionClass($bundle);
+    }
+
+    public function getMethod($method)
+    {
+        return $this->reflection->getMethod($method)->getName();
+    }
+
+    public function getParameters($method, array $arguments = array())
+    {
+        $parameters = $this->reflection->getMethod($method)->getParameters();
+
+        $i = 0;
+        $temp = $arguments;
+
+        $arguments = array();
+
+        foreach ($parameters as $index => $param) {
+            if (is_object($param->getClass())) {
+                if (!class_exists(($class = $param->getClass()->getName()))) {
+                    throw new \Exception(sprintf("Class '%s' is undefined.", $class));
+                }
+
+                $arg = new $class();
+            } else {
+                $arg = $temp[$i];
+                $i++;
+            }
+
+            $arguments[$index] = $arg;
+        }
+
+        return $arguments;
+    }
+
+    public function __call($method, $arguments)
+    {
+        if (!method_exists($this->bundle, $method)) {
+            throw new \BadMethodCallException(sprintf("Method '%s' is undefined.", $method));
+        }
+
+        $parameters = $this->getParameters($method, $arguments);
+
+        print_r($parameters);
+    }
+
+    public static function __callStatic($method, $args)
+    {}
+}
---
+++ examples/Demo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: janhuang
+ * Date: 15/1/26
+ * Time: 下午1:07
+ * Github: https://www.github.com/janhuang 
+ * Coding: https://www.coding.net/janhuang
+ * sf: http://segmentfault.com/u/janhuang
+ * Blog: http://segmentfault.com/blog/janhuang
+ */
+class Demo 
+{
+    public function test(Test $test, $b, $a, $c)
+    {
+        print_r($test);
+    }
+}
